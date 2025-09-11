efa47e555807781334283868641bd60767618583
cwinyabyui <mcwinya@gmail.com>
2025-09-11 16:22:45 +0300

    Temples updated

---
+++ temples.js
@@ -0,0 +1,3 @@
+const lastModifiedDate = document.lastModified;
+console.log("Last Mofidfied;", lastModifiedDate);
+document.getElementById("lastModified").textContent = "Last Modification: " + lastModifiedDate;
